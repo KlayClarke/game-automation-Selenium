049f324f8b25ec7e3b3ed891c524559585b3a689
Klay Clarke <klayaclarke@gmail.com>
2021-12-03 18:46:56 -0500

    access amazon homepage using selenium automation

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import selenium
+from selenium import webdriver
 
 chrome_driver_path = '/Users/klayclarke/Desktop/Development/chromedriver'
+driver = webdriver.Chrome(executable_path=chrome_driver_path)
+
+driver.get(url='https://www.amazon.com')
 
 
