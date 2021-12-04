0582958a566d31a49f46d918b0c202cd3587fe56
Klay Clarke <klayaclarke@gmail.com>
2021-12-03 23:05:07 -0500

    complete a web form (fname, lname, email) and submit said form using selenium

--- interaction.py
+++ interaction.py
@@ -5,9 +5,15 @@ from selenium.webdriver.common.keys import Keys
 chrome_driver_path = '/Users/klayclarke/Desktop/Development/chromedriver'
 driver = webdriver.Chrome(executable_path=chrome_driver_path)
 
-driver.get(url='https://en.wikipedia.org/wiki/Main_Page')
+driver.get(url='http://secure-retreat-92358.herokuapp.com/')
 
-search = driver.find_element(By.NAME, 'search')
-search.send_keys('Python')
-search.send_keys(Keys.ENTER)
+first_name = driver.find_element(By.NAME, 'fName')
+first_name.send_keys('Klay')
+last_name = driver.find_element(By.NAME, 'lName')
+last_name.send_keys('Clarke')
+email = driver.find_element(By.NAME, 'email')
+email.send_keys('testing@test.test')
+
+email.send_keys(Keys.ENTER)
+
 # driver.quit()
