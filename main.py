f7e754eba75ce1013e4537dfd0b5440998110e55
Klay Clarke <klayaclarke@gmail.com>
2021-12-03 19:45:29 -0500

    list event dates from python.org homepage

--- main.py
+++ main.py
@@ -4,10 +4,9 @@ from selenium.webdriver.common.by import By
 chrome_driver_path = '/Users/klayclarke/Desktop/Development/chromedriver'
 driver = webdriver.Chrome(executable_path=chrome_driver_path)
 
-driver.get(url='https://www.amazon.com/Neumann-87-Ai-Set-Microphone/dp/B00502CCXG/?_encoding=UTF8&pd_rd_w=MmRCm'
-               '&pf_rd_p=29505bbf-38bd-47ef-8224-a5dd0cda2bae&pf_rd_r=A16QKW2WNNQ6CQA3HHZG&pd_rd_r=ff5578c4-4c44-43fc'
-               '-87f9-c435b07d0d8c&pd_rd_wg=FrK1F&ref_=pd_gw_ci_mcx_mr_hp_atf_m')
-product_title = driver.find_element(By.ID, 'productTitle')
-print(product_title.text)
-
+driver.get(url='https://www.python.org/')
+all_dates = driver.find_elements(By.CSS_SELECTOR, 'li time')
+event_dates = all_dates[-5:]
+event_dates_text = [date.text for date in event_dates]
+print(event_dates_text)
 driver.quit()
