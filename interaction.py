fbc06fa98661811588079cdcf82859bc85fa5a3f
Klay Clarke <klayaclarke@gmail.com>
2021-12-03 22:41:10 -0500

    insert text in text field,  submit (Enter) using send_keys method

--- interaction.py
+++ interaction.py
@@ -1,15 +1,13 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
 
 chrome_driver_path = '/Users/klayclarke/Desktop/Development/chromedriver'
 driver = webdriver.Chrome(executable_path=chrome_driver_path)
 
 driver.get(url='https://en.wikipedia.org/wiki/Main_Page')
 
-article_count = driver.find_element(By.CSS_SELECTOR, '#articlecount a')
-
-# article_count.click()
-
-driver.find_element(By.LINK_TEXT, article_count.text).click()
-
+search = driver.find_element(By.NAME, 'search')
+search.send_keys('Python')
+search.send_keys(Keys.ENTER)
 # driver.quit()
