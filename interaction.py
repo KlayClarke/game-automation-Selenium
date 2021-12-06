238787ad5797ac15fc69b8798caca16cb31ff52c
Klay Clarke <klayaclarke@gmail.com>
2021-12-06 15:04:01 -0500

    retrieve user cookie count from html, compare user count to prices of available upgrades (where cookie count is greater than upgrade price)

--- interaction.py
+++ interaction.py
@@ -1,12 +1,8 @@
-import sched, time
-from threading import Timer
-import selenium.webdriver
+import time
 from selenium import webdriver
 from selenium.webdriver.common.by import By
-from selenium.webdriver.common.keys import Keys
-from selenium.common.exceptions import StaleElementReferenceException, ElementNotInteractableException
-
-s = sched.scheduler(time.time, time.sleep)
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.common.exceptions import StaleElementReferenceException, NoSuchElementException
 
 chrome_driver_path = '/Users/klayclarke/Desktop/Development/chromedriver'
 driver = webdriver.Chrome(executable_path=chrome_driver_path)
@@ -17,7 +13,6 @@ driver.get(url='http://orteil.dashnet.org/experiments/cookie/')
 cookie = driver.find_element(By.ID, 'cookie')
 
 upgrades = {}
-upgrade_prices = {}
 
 # try to put all upgrade attributes in one big dict
 list_of_upgrades = driver.find_elements(By.CSS_SELECTOR, '#store div')
@@ -28,23 +23,50 @@ for upgrade in list_of_upgrades:
     try:
         upgrade_price = int(upgrade.text.split('\n')[0].split('-')[1])
     except ValueError:
-        upgrade_price = upgrade.text.split('\n')[0].split('-')[1].replace(',', '')
+        upgrade_price = int(upgrade.text.split('\n')[0].split('-')[1].replace(',', ''))
     except IndexError:
-        upgrade_price = 0
-    print(upgrade_price)
+        upgrade_price = 1000000000
     # place upgrade ids and prices in dict
     upgrade_dict_entry = {
-        upgrade_id: {
-            'price': upgrade_price,
-            'element': upgrade
+        upgrade_price: {
+            'id': upgrade_id,
         }
     }
     upgrades.update(upgrade_dict_entry)
 
+in_five_sec = time.time() + 5
+in_five_min = time.time() + 300
 
-print(upgrades)
+run_game = True
 
-# driver.quit()
+# run game - click nonstop
+while run_game:
+    cookie.click()
+
+    # this code should run every 5 seconds
+    if time.time() > in_five_sec:
+        upgrade_prices = []
+        # get user cookie count
+        try:
+            cookie_count = int(driver.find_element(By.ID, 'money').text)
+        except ValueError:
+            cookie_count = int(driver.find_element(By.ID, 'money').text.replace(',', ''))
+        # compare cookie count to upgrade prices
+        for key in upgrades.keys():
+            key_price = key
+            if cookie_count > key_price:
+                upgrade_prices.append(key_price)
+    #     # click most expensive available upgrade
+    #     most_expensive_available_upgrade = upgrade_prices[-1]
+    #     time.sleep(1)
+    #     most_expensive_id = upgrades[most_expensive_available_upgrade]['id']
+    #     button = driver.find_element(By.ID, most_expensive_id)
+    #     button.click()
+    #     in_five_sec = time.time() + 5
+    #
+    # if time.time() > in_five_min:
+    #     run_game = False
+    #     driver.quit()
 
 # TODO: get upgrade item ids
 # TODO: get all upgrade items b tags - isolate price
